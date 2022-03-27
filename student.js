09ebfc6644562a844da49133e77f82e5bdbe8d4a
Sarah Flodin Jakobsson <sarahflodinjakobsson@gmail.com>
2022-03-27 16:20:24 +0200

    update viewStudents

--- student.js
+++ student.js
@@ -19,3 +19,53 @@ function viewStrudent(id) {
         </div>`
     
 }
+<<<<<<< Updated upstream
+=======
+function viewCourses(students)
+    let studentsElement = document.getElementById("students");
+    for (let student of students) {
+        let studentElement = viewStudent(student.studentID);
+        studentElement.appendChild(studentElement);
+    }
+
+function viewCourses(student){
+    let courseInfo = DATABASE.courses;
+    let courses = [];
+    for (let i = 0; i < student.length; i++) {
+        let id = student.courses[i].courseId;
+        courses.push(courseInfo[id]);
+    }
+}
+
+    let courseForm = [];
+    for (let i = 0; i < courses.length; i++) {
+        let div = document.createElement("div");
+    }
+
+    if (student.courses[i].passesCredits == courseInfo[courses[i].courseId].totalCredits){
+        let info = div.innerHTML = `
+            <div class="done"> 
+            <h3>${courses[i].title}</h3>
+            <p>${student.courses[i].started.semester}
+            ${student.courses[i].started.year}
+            (${student.courses[i].passesCredits} of 
+            ${courseInfo[courses[i].courseId].totalCredits} credits)</p>
+            </div>`
+            courseForm.push(info); 
+        } else {
+            let info = div.innerHTML = `
+            <div class="notDone">
+            <h3> ${courses[i].title} </h3>
+            <p>${student.courses[i].started.semester}
+        ${student.courses[i].started.year}
+        (${student.courses[i].passedCredits} of ${courseInfo[courses[i].courseId].totalCredits} credits)
+        <p/>
+        </div>`
+
+        courseForm.push(info);
+    }
+}
+    return courseForm.toString().split(",").join("");
+
+}
+>>>>>>> Stashed changes
